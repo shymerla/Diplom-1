3b9202c36eaa528e98383c5ad79f7572d025048b
Anatolii Nikolaev <snowliona@yandex.ru>
2022-06-11 17:46:18 +0300

    +1

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import praktikum.Bun;
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    private final String bunName = "crazy bun";
+    private final float bunPrice = 111f;
+
+    @Test
+    public void getNameOfBunCorrect() {
+        Bun bun =new Bun(bunName,bunPrice);
+        String actualName = bun.getName();
+        assertEquals("Name of bun ERROR", bunName, actualName);
+    }
+
+    @Test
+    public void getPriceOfBunCorrect() {
+        Bun bun =new Bun(bunName,bunPrice);
+        float actualPrice = bun.getPrice();
+        assertEquals("Price of bun ERROR", bunPrice, actualPrice, 0);
+    }
+
+
+
+
+}
